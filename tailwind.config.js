f24797234b441ef894f9382a56f271f191c4d5ae
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2023-11-04 14:28:18 +0330

    adding coustom color and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,20 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily:{
+      'kumbhRgular':["'kumbh-rgular'"],
+      'kumbhMedium':["'kumbh-medium'"],
+      'kumbhSemiBold':["'kumbh-semiBold'"],
+      'kumbhBold':["'kumbh-bold'"],
+    },
+    colors:{
+      'orange':'hsl(26, 100%, 55%)',
+      'paleOrange':'hsl(25, 100%, 94%)',
+      'veryDarkGrayish':'hsl(220, 13%, 13%)',
+      'darkGrayishBlue':'hsl(219, 9%, 45%)',
+      'grayishBlue':'hsl(220, 14%, 75%)',
+      
+    },
     extend: {},
   },
   plugins: [],
