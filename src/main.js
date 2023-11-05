f3c5ea4d270efc2456846551cafdb7c39c571e75
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2023-11-05 18:46:12 +0330

    pinia state added

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import './assets/global.scss'
+import { createPinia } from 'pinia'
 
-createApp(App).mount('#app')
+const pinia = createPinia()
+
+createApp(App).use(pinia).mount('#app')
