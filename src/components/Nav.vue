a49516c9c0528c419ca8eebdae7a56babeae90ec
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2023-11-05 18:06:34 +0330

    mobile section seprated and optimizng for small sizes

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,10 +1,16 @@
 <script setup>
-import Cart from './Cart.vue';
+import MobileNav from './MobileNav.vue'
+import Cart from './Cart.vue'
+import { ref } from 'vue';
+
+let trigerCartFlag = ref(false)
+
 </script>
 <template>
-    <div class="2xl:w-10/12  h-[120px] flex items-center justify-between border-b-[1px] border-b-grayishBlue">
+    <div
+        class="2xl:w-10/12 w-11/12  md:h-[120px] h-[90px] flex items-center justify-between lg:border-b-[1px] border-b-grayishBlue">
         <!-- desktop nav -->
-        <div class="flex gap-10 items-center">
+        <div class="lg:flex hidden gap-10 items-center">
             <img src="/images/logo.svg" alt="site logo">
             <ul class="flex text-darkGrayishBlue  font-kumbhRgular">
                 <li><a href="#" class="navItem">Collections</a></li>
@@ -14,16 +20,20 @@ import Cart from './Cart.vue';
                 <li><a href="#" class="navItem">Contact</a></li>
             </ul>
         </div>
+        <!-- mobile nav -->
+        <MobileNav />
         <!-- cart and profile -->
-        <div class="flex gap-5  items-center">
-            <div class="relative w-10"><img src="/images/icon-cart.svg" class="w-6 h-6 cursor-pointer" alt="cart icon">
-                <p class="absolute bg-orange text-[white] xl:text-sm px-2 rounded-full xl:top-[-10px] right-2">2</p>
+        <div class="flex md:gap-5 gap-2  items-center">
+            <div class="relative w-10"><img src="/images/icon-cart.svg" class="w-6 h-6 cursor-pointer " alt="cart icon"
+                    @mouseenter="trigerCartFlag = !trigerCartFlag" @mouseleave="trigerCartFlag = !trigerCartFlag">
+                <p class="absolute bg-orange text-[white] text-sm md:px-2 px-2 rounded-full top-[-10px]  right-2 ">2
+                </p>
+                <!-- avatar -->
             </div>
             <div class=""> <img src="/images/image-avatar.png" class="profileImage" alt="cart icon">
             </div>
-
         </div>
         <!-- cart bascket -->
-        <Cart />
+        <Cart v-if="trigerCartFlag" />
     </div>
 </template>
