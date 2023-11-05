95e6392777af9ae60b0307a3ec2c6cb746f834dc
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2023-11-05 11:06:05 +0330

    sprated cart and imported

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,4 +1,5 @@
 <script setup>
+import Cart from './Cart.vue';
 </script>
 <template>
     <div class="2xl:w-10/12  h-[120px] flex items-center justify-between border-b-[1px] border-b-grayishBlue">
@@ -14,9 +15,15 @@
             </ul>
         </div>
         <!-- cart and profile -->
-        <div class="flex gap-10 items-center">
-            <img src="/images/icon-cart.svg" class="w-6 h-6 cursor-pointer" alt="cart icon">
-            <img src="/images/image-avatar.png" class="profileImage" alt="cart icon">
+        <div class="flex gap-5  items-center">
+            <div class="relative w-10"><img src="/images/icon-cart.svg" class="w-6 h-6 cursor-pointer" alt="cart icon">
+                <p class="absolute bg-orange text-[white] xl:text-sm px-2 rounded-full xl:top-[-10px] right-2">2</p>
+            </div>
+            <div class=""> <img src="/images/image-avatar.png" class="profileImage" alt="cart icon">
+            </div>
+
         </div>
+        <!-- cart bascket -->
+        <Cart />
     </div>
 </template>
