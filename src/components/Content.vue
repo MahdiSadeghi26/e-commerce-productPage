f2a3a5ab73b62e4f94207640777efd7a8ef7dc5e
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2023-11-27 12:40:37 +0330

    removed extra imports and comments updated

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,17 +1,16 @@
 <script setup>
-import { ref } from 'vue'
 import ImageModal from './ImageModal.vue';
 import { useModalStore } from '../stores/Modal'
 import ProductImage from './ProductImage.vue';
-let clickedImageIndex = ref(1)
+import ProductInfo from './ProductInfo.vue';
 let ModalStore = useModalStore()
 </script>
 <template>
     <div class="flex xl:w-9/12 mt-10 py-5 h-3/5">
         <!-- left side product image and modal -->
-        <ProductImage/>
-        <!-- right side order seciton  -->
-        <div class=""></div>
+        <ProductImage />
+        <!--product info and order  -->
+        <ProductInfo/>
     </div>
     <!-- modal -->
     <ImageModal v-if="ModalStore" />
