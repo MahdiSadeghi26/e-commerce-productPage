19f02457873813fd116580d5b8da322420ad367f
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2023-11-27 12:42:18 +0330

    product image turned into cmp

---
+++ src/components/ProductImage.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { useImageIndexStore } from '../stores/ImageIndex'
+import { useModalStore } from '../stores/Modal'
+
+let ModalStore = useModalStore()
+let activeImage = useImageIndexStore()
+const imageClickHandler = (index) => {
+    ModalStore.isActive = true
+    activeImage.activeIndex = index
+}
+</script>
+<template>
+    <div class="flex flex-col gap-y-5 w-1/2 items-center h-5/6 xl:px-10 ">
+        <!-- main image -->
+        <img src="/images/image-product-1.jpg" alt="main picture" class="w-10/12  rounded-2xl">
+        <div class="w-10/12 flex gap-8 ">
+            <div class="smallPicture" @click="imageClickHandler(1)" :class="{ activeItem: activeImage.activeIndex == 1 }">
+                <img src="/images/image-product-1.jpg" alt="main picture" class="rounded-xl"
+                    :class="{ activePicture: activeImage.activeIndex == 1 }">
+            </div>
+            <div class="smallPicture" @click="imageClickHandler(2)" :class="{ activeItem: activeImage.activeIndex == 2 }">
+                <img src="/images/image-product-2.jpg" alt="main picture" class="rounded-xl "
+                    :class="{ activePicture: activeImage.activeIndex == 2 }">
+            </div>
+            <div class="smallPicture" @click="imageClickHandler(3)" :class="{ activeItem: activeImage.activeIndex == 3 }">
+                <img src="/images/image-product-3.jpg" alt="main picture" class="rounded-xl "
+                    :class="{ activePicture: activeImage.activeIndex == 3 }">
+            </div>
+            <div class="smallPicture" @click="imageClickHandler(4)" :class="{ activeItem: activeImage.activeIndex == 4 }">
+                <img src="/images/image-product-4.jpg" alt="main picture" class="rounded-xl "
+                    :class="{ activePicture: activeImage.activeIndex == 4 }">
+            </div>
+
+        </div>
+    </div>
+</template>
+
