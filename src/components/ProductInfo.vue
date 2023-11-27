91466d1de661e909e5f8d9d2ea4e017894d18669
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2023-11-27 16:43:46 +0330

    md size ready

--- src/components/ProductInfo.vue
+++ src/components/ProductInfo.vue
@@ -2,7 +2,7 @@
 
 </script>
 <template>
-    <div class="xl:p-10 xl:w-[550px] lg:w-1/2 flex flex-col gap-7 justify-center lg:px-5">
+    <div class="xl:p-10 xl:w-[550px] md:w-[450px] flex flex-col gap-7  lg:px-5">
             <!-- company name -->
             <h2 class="uppercase text-orange font-kumbhBold tracking-widest">sneakers company</h2>
             <!-- product title -->
@@ -13,9 +13,9 @@
                 they’ll withstand everything the weather can offer.
             </p>
             <!-- price section -->
-            <div class="flex flex-col gap-1">
+            <div class="flex lg:flex-col justify-between gap-1">
                 <!-- current price and off precent -->
-                <div class="flex lg:gap-5 items-center">
+                <div class="flex gap-5 items-center">
                     <p class="text-black font-kumbhBold tracking-wide text-3xl">$125.00</p>
                     <!-- off precent -->
                     <p class="bg-paleOrange text-orange px-2 py-1 rounded-lg font-kumbhBold">50%</p>
@@ -24,13 +24,13 @@
                 <p class="text-grayishBlue line-through text-lg font-kumbhBold">$250.00</p>
             </div>
             <!-- add to cart section -->
-            <div class="flex lg:gap-4">
-                <div class="bg-lightgb w-5/12 text-center flex justify-between items-center px-4 py-4 rounded-md">
+            <div class="flex lg:flex-row flex-col lg:gap-4 md:gap-y-5">
+                <div class="bg-lightgb lg:w-5/12 w-full text-center flex justify-between items-center px-4 py-4 rounded-md">
                     <i class="fa-solid fa-minus text-orange cursor-pointer"></i>
                     <p class="font-kumbhBold">0</p>
                     <i class="fa-solid fa-plus text-orange cursor-pointer"></i>
                 </div>
-                <div class="w-3/5 bg-orange rounded-lg flex items-center justify-center gap-3">
+                <div class="lg:w-3/5 w-full lg:h-auto md:h-[70px] bg-orange rounded-lg flex items-center justify-center gap-3">
                     <i class="fa-solid fa-cart-shopping" style="color: #ffffff;"></i>
                     <p class="text-white text-lg font-kumbhMedium">Add to cart</p>
                 </div>
