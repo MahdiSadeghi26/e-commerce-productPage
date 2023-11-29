751b4a6a17107822960f3ff210027796d0d5ed24
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2023-11-29 11:25:19 +0330

    slide funcitons added

--- src/components/ProductImage.vue
+++ src/components/ProductImage.vue
@@ -8,12 +8,29 @@ const imageClickHandler = (index) => {
     ModalStore.isActive = true
     activeImage.activeIndex = index
 }
+const nextSlide = () => {
+    if (activeImage.activeIndex < 4) {
+        activeImage.activeIndex++
+    } else {
+        activeImage.activeIndex = 1
+    }
+}
+const prevSlide = () => {
+    if (activeImage.activeIndex>1) {
+        activeImage.activeIndex--
+    }else{
+        activeImage.activeIndex=4
+    }
+}
 </script>
 <template>
-    <div class="flex flex-col gap-y-5 lg:w-1/2 md:w-full  items-center lg:h-5/6 xl:px-10 ">
+    <div class="flex flex-col gap-y-5 lg:w-1/2 md:w-full  items-center  xl:px-10 md:relative ">
         <!-- main image -->
-        <img src="/images/image-product-1.jpg" alt="main picture" class="lg:w-10/12 md:h-[300px] w-full  md:rounded-2xl">
-        <div class="w-10/12 lg:flex hidden gap-8  ">
+        <img :src="`/images/image-product-${activeImage.activeIndex}.jpg`" :key="activeImage.activeIndex" alt="main picture"
+            class="lg:w-10/12 lg:h-full md:h-[300px] w-full  md:rounded-2xl ">
+        <i class="fa-solid fa-chevron-left fa-xl  md:top-[40%] md:left-0 mobileSlide" @click="prevSlide"></i>
+        <i class="fa-solid fa-chevron-right fa-xl md:top-[40%] md:right-0 mobileSlide" @click="nextSlide"></i>
+        <div class="w-10/12 lg:flex hidden gap-8 lg:h-2/6  ">
             <div class="smallPicture" @click="imageClickHandler(1)" :class="{ activeItem: activeImage.activeIndex == 1 }">
                 <img src="/images/image-product-1.jpg" alt="main picture" class="rounded-xl"
                     :class="{ activePicture: activeImage.activeIndex == 1 }">
