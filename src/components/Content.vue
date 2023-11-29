84eaad193e86817a71a6ced6d847725bea9dccc5
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2023-11-29 11:25:00 +0330

    removing extra imprort and size optimized

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,12 +1,11 @@
 <script setup>
-import ImageModal from './ImageModal.vue';
 import { useModalStore } from '../stores/Modal'
 import ProductImage from './ProductImage.vue';
 import ProductInfo from './ProductInfo.vue';
 let ModalStore = useModalStore()
 </script>
 <template>
-    <div class="flex lg:flex-row flex-col lg:gap-0 gap-y-5 items-center xl:w-9/12 lg:w-11/12  mt-10 py-5 h-3/5">
+    <div class="flex lg:flex-row flex-col lg:gap-0 gap-y-5 items-center xl:w-9/12  mt-10 py-5 h-4/5">
         <!-- left side product image and modal -->
         <ProductImage />
         <!--product info and order  -->
