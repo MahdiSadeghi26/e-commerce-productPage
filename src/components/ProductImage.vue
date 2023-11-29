3b4e959e3c5080fc6ddbebe1a1634f5b0b5ba8f6
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2023-11-29 15:25:42 +0330

    fixing z index and formatting

--- src/components/ProductImage.vue
+++ src/components/ProductImage.vue
@@ -16,20 +16,20 @@ const nextSlide = () => {
     }
 }
 const prevSlide = () => {
-    if (activeImage.activeIndex>1) {
+    if (activeImage.activeIndex > 1) {
         activeImage.activeIndex--
-    }else{
-        activeImage.activeIndex=4
+    } else {
+        activeImage.activeIndex = 4
     }
 }
 </script>
 <template>
-    <div class="flex flex-col gap-y-5 lg:w-1/2 md:w-full  items-center  xl:px-10 md:relative -z-10 ">
+    <div class="flex flex-col gap-y-5 lg:w-1/2 w-full md:h-auto  items-center h-[270px]  xl:px-10 md:relative  ">
         <!-- main image -->
         <img :src="`/images/image-product-${activeImage.activeIndex}.jpg`" :key="activeImage.activeIndex" alt="main picture"
-            class="lg:w-10/12 lg:h-full md:h-[300px] w-full  md:rounded-2xl ">
-        <i class="fa-solid fa-chevron-left fa-xl  md:top-[40%] md:left-0 mobileSlide" @click="prevSlide"></i>
-        <i class="fa-solid fa-chevron-right fa-xl md:top-[40%] md:right-0 mobileSlide" @click="nextSlide"></i>
+            class="lg:w-10/12 lg:h-full md:h-[300px] h-full  w-full  md:rounded-2xl" >
+        <i class="fa-solid fa-chevron-left fa-xl md:left-0 left-4 mobileSlide" @click="prevSlide"  ></i>
+        <i class="fa-solid fa-chevron-right fa-xl md:right-0 right-4 mobileSlide" @click="nextSlide"></i>
         <div class="w-10/12 lg:flex hidden gap-8 lg:h-2/6  ">
             <div class="smallPicture" @click="imageClickHandler(1)" :class="{ activeItem: activeImage.activeIndex == 1 }">
                 <img src="/images/image-product-1.jpg" alt="main picture" class="rounded-xl"
