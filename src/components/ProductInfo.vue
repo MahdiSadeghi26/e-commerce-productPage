8cf38b9853e4aba93d36f3996054594b0592b529
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2023-11-30 17:34:12 +0330

    adding functions

--- src/components/ProductInfo.vue
+++ src/components/ProductInfo.vue
@@ -1,5 +1,25 @@
 <script setup>
+import { ref } from 'vue'
+import { useCartStore } from '../stores/cart';
+ let productCount=ref(0)
+ let cart=useCartStore()
+ const decrementCount=()=>{
+   if (productCount.value>=1) {
+    productCount.value--
+   }else{
+    productCount=0
+   }
+ }
+ const createOrder=(count)=>{
+    if (!cart.count) {
+        cart.count=count
+    }else if (cart.count && count) {
+        cart.count+=count
+    }else{
+        return
+    }
 
+ }
 </script>
 <template>
     <div class="xl:p-10 xl:w-[550px] lg:h-5/6 md:w-[450px] flex flex-col gap-7  lg:px-5 px-5 pb-10" >
@@ -26,13 +46,13 @@
         <!-- add to cart section -->
         <div class="flex lg:flex-row flex-col lg:gap-4 gap-y-5 ">
             <div class="counter">
-                <i class="fa-solid fa-minus text-orange cursor-pointer"></i>
-                <p class="font-kumbhBold">0</p>
-                <i class="fa-solid fa-plus text-orange cursor-pointer"></i>
+                <i class="fa-solid fa-minus text-orange cursor-pointer" @click="decrementCount"></i>
+                <p class="font-kumbhBold">{{productCount}}</p>
+                <i class="fa-solid fa-plus text-orange cursor-pointer" @click="productCount++"></i>
             </div>
-            <div class="lg:w-3/5 w-full lg:h-auto md:h-[70px] h-[55px] bg-orange rounded-lg flex items-center justify-center gap-3 cursor-pointer">
+            <div class="lg:w-3/5 w-full lg:h-auto md:h-[70px] h-[55px] bg-orange rounded-lg flex items-center justify-center gap-3 cursor-pointer" @click="createOrder(productCount)">
                 <i class="fa-solid fa-cart-shopping" style="color: #ffffff;"></i>
-                <p class="text-white text-lg font-kumbhMedium">Add to cart</p>
+                <p class="text-white text-lg font-kumbhMedium" >Add to cart</p>
             </div>
         </div>
     </div>
