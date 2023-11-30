897e4eed6f616cb48bb14d1c75d1644031d4158b
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2023-11-30 17:31:54 +0330

    adding passive close btn to content

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -2,10 +2,12 @@
 import { useModalStore } from '../stores/Modal'
 import ProductImage from './ProductImage.vue';
 import ProductInfo from './ProductInfo.vue';
+import { useCartStore } from '../stores/cart';
 let ModalStore = useModalStore()
+let cart=useCartStore()
 </script>
 <template>
-    <div class="flex lg:flex-row flex-col lg:gap-0 gap-y-5 items-center xl:w-9/12  md:mt-10 md:py-5 h-4/5">
+    <div class="flex lg:flex-row flex-col lg:gap-0 gap-y-5 items-center xl:w-9/12  md:mt-10 md:py-5 h-4/5" @click="cart.isActive=false">
         <!-- left side product image and modal -->
         <ProductImage />
         <!--product info and order  -->
